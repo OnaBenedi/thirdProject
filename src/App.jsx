9f9ea815e221d3798b1e85d503ce6765dd6d7d89
OnaBenedi <onabenedi@gmail.com>
2024-06-21 10:50:43 +0200

    touched some details

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import "./App.css";
-import Header from "./components/header/Header";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import UserProfile from "./components/user-profile/UserProfile";
-import UserTrips from "./components/user-trips/UserTrips";
-import Configuracion from "./components/configuracion/Configuracion";
 import Home from "./components/home/Home";
-import Help from "./components/help/Help";
+
 
 function App() {
   return (
