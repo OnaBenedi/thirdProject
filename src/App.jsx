8df34c93f7a2d591668330dd73b91ba8f16ef1de
OnaBenedi <onabenedi@gmail.com>
2024-06-28 11:18:02 +0200

    fixed missing imports from first merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./components/home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserProfile from "./components/user-profile/UserProfile";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home/>}/>
+        <Route path="home" element={<Home/>}/>
         <Route path="profile" element={<UserProfile/>}/>
         </Routes>      
     </BrowserRouter>
