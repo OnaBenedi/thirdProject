7331ea680c8a1507afab1f4a01b905bdcd2c386b
Christ J. <christ.jmanzano@gmail.com>
2024-07-12 10:36:30 +0100

    add comparator component

---
+++ src/components/pages/Comparator.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+
+function Comparator() {
+  return (
+    <>
+        <Header></Header>
+      <div>
+        <img src="src\assets\img\map-placeholder.webp" alt="Map Placeholer" />
+      </div>
+    </>    
+  )
+}
