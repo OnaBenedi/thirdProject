b24ba97edab49dd67ba28e11bc55bfab7452d57d
María Bona <mariabonaenguita@gmail.com>
2024-07-16 11:32:15 +0200

    config styled and media queries added

--- src/components/configuracion/configuration-body/ConfigurationBody.jsx
+++ src/components/configuracion/configuration-body/ConfigurationBody.jsx
@@ -1,26 +1,26 @@
-
+import "./ConfigurationBody.css"
 
 function ConfigurationBody() {
   function handleUpdate(e) {
     e.prevent.default();
   }
   return (
-    <div>
+    <div className="config-container">
       <h2>Ajustes</h2>
       <form onSubmit={handleUpdate}>
-        <div>
+        <div className="input-container">
           <label>
             Username:
             <input type="text" name="username" />
           </label>
         </div>
-        <div>
+        <div className="input-container">
           <label>
             Email:
             <input type="email" name="email" />
           </label>
         </div>
-        <div>
+        <div className="input-container">
           <label>
             Password:
             <input type="password" name="password" />
