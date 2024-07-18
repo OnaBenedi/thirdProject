535e52f150b7b29d383202c70aad1fad646ae7de
OnaBenedi <onabenedi@gmail.com>
2024-07-18 15:07:20 +0200

    started trips database

--- src/components/header/hamburguer-menu/HamburguerMenu.jsx
+++ src/components/header/hamburguer-menu/HamburguerMenu.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import './HamburguerMenu.css';
-import mainLogo from "../../../assets/logos/main-logo.png"
+import React, { useState } from "react";
+import "./HamburguerMenu.css";
+import mainLogo from "../../../assets/logos/main-logo.png";
+import { Link } from "react-router-dom";
 
 function HamburguerMenu() {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,16 +24,24 @@ function HamburguerMenu() {
         </div>
       )}
       {isOpen && (
-        <nav className={`menu ${isOpen ? 'open' : ''}`}>
-        <img src={mainLogo} alt="logo-small" className="main-logo-small"/> 
+        <nav className={`menu ${isOpen ? "open" : ""}`}>
+          <img src={mainLogo} alt="logo-small" className="main-logo-small" />
           <button className="close-button" onClick={closeMenu}>
             <span>&times;</span>
           </button>
           <ul>
-            <li><a href="/home">Inicio</a></li>
-            <li><a href="/profile">Perfil</a></li>
-            <li><a href="/trips">Viajes</a></li>
-            <li><a href="/config">Ajustes</a></li>
+            <li>
+              <Link to={"/home"}>Inicio</Link>
+            </li>
+            <li>
+              <Link to={"/profile"}>Perfil</Link>
+            </li>
+            <li>
+              <Link to={"/trips"}>Viajes</Link>
+            </li>
+            <li>
+              <Link to={"/config"}>Ajustes</Link>
+            </li>
           </ul>
         </nav>
       )}
@@ -41,6 +50,3 @@ function HamburguerMenu() {
 }
 
 export default HamburguerMenu;
-
-
-
--- src/components/trips/trips-body/TripsBody.jsx
+++ src/components/trips/trips-body/TripsBody.jsx
@@ -1,29 +1,47 @@
 import "./TripsBody.css"
+import tripData from "../../../data/tripData"
 
 function TripsBody() {
+
+  const restaurantes = tripData.madrid.restaurantes;
+  const hoteles = tripData.madrid.hoteles;
+  const estaciones = tripData.madrid.estaciones;
   return (
     <div className="trips-container"  >
+      Currently under development!
+      <h1>Madrid</h1>
       <div className="trips">
-        <h1>Madrid</h1>
-        <h3>Restaurantes</h3>
-        <ul>
-          <li>Biang Biang Bar</li>
-          <li>L'Entrecote Café de París</li>
-          <li>Hermanos Vinagre</li>
-          <li>El viajero</li>
-        </ul>
-        <h3>Hoteles</h3>
-        <ul>
-          <li>Eurostars Madrid Tower</li>
-          <li>Hotel Madrid Gran Vía</li>
-          <li>Hotel Palacio del Retiro</li>
-        </ul>
-        <h3>Estaciones</h3>
-        <ul>
-          <li>Puerta de Atocha-Almudena Grandes</li>
-          <li>Chamartín</li>
-          <li>Príncipe Pío</li>
-        </ul>
+
+        <div className="restaurants">
+          <h3>Restaurantes</h3>
+          <ul>
+            {tripData ? restaurantes.map((restaurante, index)=>{
+              return <div key={index}>
+                <li>{restaurante}</li>
+              </div>
+            }) : null}
+          </ul>
+        </div>
+        <div className="hotels">
+          <h3>Hoteles</h3>
+          <ul>
+          {tripData ? hoteles.map((hotel, index)=>{
+              return <div key={index}>
+                <li>{hotel}</li>
+              </div>
+            }) : null}
+          </ul>
+        </div>
+        <div className="train-stations">
+          <h3>Estaciones</h3>
+          <ul>
+          {tripData ? estaciones.map((estacion, index)=>{
+              return <div key={index}>
+                <li>{estacion}</li>
+              </div>
+            }) : null}
+          </ul>
+        </div>
       </div>
     </div>
   );
