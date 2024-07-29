cea6c31d9b7a279bf691b35b60e912020b0b61f6
Christopher <161458607+chokchrist@users.noreply.github.com>
2024-07-29 19:55:50 +0100

    fix design

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -2,36 +2,46 @@ import "./Login.css";
 import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../config/firebase";
-import { useNavigate } from 'react-router-dom';
-import Footer from "../footer/Footer"
+import { useNavigate } from "react-router-dom";
+import "./Password";
+import Footer from "../footer/Footer";
+import Password from "./Password";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPasswordReset, setShowPasswordReset] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      navigate("/home")
+      navigate("/home");
     } catch (err) {
       console.error(err.message);
     }
   };
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    setShowPasswordReset(!showPasswordReset);
+  };
+
   return (
     <div className="Login">
-      <div className="container">
+      {showPasswordReset && <Password exitFunction={handleClick}/>}
+
+      <div className="container-login">
         <div className="left">
-          <h2>Create an account</h2>
+          <h2 className="signup-title-left">Create an account</h2>
           <button className="signInButton" onClick={() => navigate("/signup")}>
             Sign up
           </button>
         </div>
         <div className="right">
-          <h2>Welcome</h2>
-          <form onSubmit={handleSubmit}>
+          <h2 className="login-title-right">Welcome</h2>
+          <form onSubmit={handleSubmit} className="login-form">
             <input
               className="login-input"
               type="email"
@@ -50,12 +60,12 @@ function Login() {
               Log in
             </button>
           </form>
-          <a href="#" className="resetPassword">
+          <a href="#" className="resetPassword" onClick={handleClick}>
             Reset password
           </a>
         </div>
       </div>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
